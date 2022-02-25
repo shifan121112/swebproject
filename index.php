01b9057585c6edc79061289dd826e081dca32693
Shifan  Hussain <36062176+shifan121112@users.noreply.github.com>
2022-02-25 18:19:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-Help 
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {background-color: powderblue;}
+h1   {color: blue;}
+p    {color: red;}
+</style>
+</head>
+<body>
+
+<h1>Dua For Ukrain</h1>
+<p> YA ALLAH PLEASE SAVE INNOCENT PEOPLE OF UKRAIN
+               PLEASE END THE WAR
+               PLEASE THE CHILDREN
+               PLEASSE SAVE OLD
+               ITS MY HUMBLE REQEST
+</p>
+
+</body>
+</html>
